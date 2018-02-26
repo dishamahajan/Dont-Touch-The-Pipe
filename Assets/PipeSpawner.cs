0241c4ca43f7ec8486e9cb0d5d205032b2c9cd0d
akashdoshi5 <akashdoshi5@gmail.com>
2018-02-26 22:52:29 +0530

    First commit

---
+++ Assets/PipeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour {
+
+	public float maxYpos;
+	public float SpawnTime;
+	public GameObject pipe;
+
+	void Start () {
+		//StartSpawningPipes ();
+	}
+
+	void Update () {
+		
+	}
+
+	public void StopSpawningPipes(){
+		CancelInvoke ("SpawnPipe");
+	}
+
+	public void StartSpawningPipes(){
+		InvokeRepeating ("SpawnPipe", 0.2f, SpawnTime);
+	}
+
+	void SpawnPipe(){
+		Instantiate (pipe, new Vector3 (transform.position.x, Random.Range (-maxYpos, maxYpos), 0), Quaternion.identity);
+	}
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	public static UIManager instance;
+	public Text scoreText;
+	public Text highScoreText;
+	public GameObject gameOverPanel;
+	public GameObject startUI;
+	public GameObject gameOverText;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = ScoreManager.instance.score.ToString ();
+	}
+
+	public void GameStart(){
+		startUI.SetActive (false);
+	}
+
+	public void GameOver(){
+		gameOverText.SetActive (true);
+		gameOverPanel.SetActive (true);
+		highScoreText.text = "High Score: "+PlayerPrefs.GetInt ("HighScore");
+	}
+
+	public void Replay(){
+		SceneManager.LoadScene ("level1");
+	}
+	public void Menu(){
+		SceneManager.LoadScene ("Menu");
+	}
+}
